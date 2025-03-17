76837430fbd2ede43e88b81babd38ca94d22053d
Geoffrey Lawson <geoffrey.lawson0@gmail.com>
2025-03-17 21:11:50 +0900

    Add Main Menu

---
+++ scripts/options_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+const OFF_SCREEN = Vector2(1100,0)
+const ON_SCREEN = Vector2(0,0)
+
+
+func _on_back_button_pressed() -> void:
+	position = OFF_SCREEN
+
+
+func _on_options_button_pressed() -> void:
+	position = ON_SCREEN
